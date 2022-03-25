89bfc8e48f8368b4a08aec004c1cac71dff263f7
toshimaru <me@toshimaru.net>
2022-03-25 23:25:17 +0900

    Refactoring test

--- src/client.test.ts
+++ src/client.test.ts
@@ -2,33 +2,46 @@ import { Client } from './client'
 
 const client = new Client('xxx.backlog.com', 'dummy_key')
 
-test('containsBacklogUrl', () => {
-  expect(client.containsBacklogUrl('')).toBe(false)
-  expect(client.containsBacklogUrl('xxx.backlog.com')).toBe(false)
-  expect(client.containsBacklogUrl('xxx.backlog.com/view/')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/PROJECT')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/PROJECT')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/PROJECT1')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/PROJECT-')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/-1')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/1-X')).toBe(false)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/1-1')).toBe(true)
-  expect(client.containsBacklogUrl('https://xxx.backlog.com/view/PROJECT-1')).toBe(true)
+describe('containsBacklogUrl', () => {
+  it.concurrent.each([
+    '',
+    'xxx.backlog.com',
+    'xxx.backlog.com/view/',
+    'https://xxx.backlog.com/view/',
+    'https://xxx.backlog.com/view/PROJECT',
+    'https://xxx.backlog.com/view/PROJECT',
+    'https://xxx.backlog.com/view/PROJECT1',
+    'https://xxx.backlog.com/view/PROJECT-',
+    'https://xxx.backlog.com/view/-1',
+    'https://xxx.backlog.com/view/1-X'
+  ])('does not contain Backlog URL', (invalidUrl) => {
+    expect(client.containsBacklogUrl(invalidUrl)).toBe(false)
+  })
+
+  it('contains Backlog URL', () => {
+    expect(client.containsBacklogUrl('https://xxx.backlog.com/view/1-1')).toBe(true)
+    expect(client.containsBacklogUrl('https://xxx.backlog.com/view/PROJECT-1')).toBe(true)
+  })
 })
 
-test('parseBacklogUrl', () => {
-  expect(client.parseBacklogUrl('')).toStrictEqual([])
-  const url = 'https://xxx.backlog.com/view/PROJECT-1'
-  expect(client.parseBacklogUrl(`URL: ${url} `)).toStrictEqual([url, 'PROJECT', 'PROJECT-1'])
+describe('parseBacklogUrl', () => {
+  test('single URL', () => {
+    expect(client.parseBacklogUrl('')).toStrictEqual([])
+    const url = 'https://xxx.backlog.com/view/PROJECT-1'
+    expect(client.parseBacklogUrl(`URL: ${url} `)).toStrictEqual([url, 'PROJECT', 'PROJECT-1'])
+  })
 })
 
-test('validateProject', async () => {
-  const isValid = await client.validateProject('')
-  expect(isValid).toBe(false)
+describe('validateProject', () => {
+  test('invalid project', async () => {
+    expect(await client.validateProject('')).toBe(false)
+    expect(await client.validateProject('INVALID')).toBe(false)
+  })
 })
 
-test('updateIssuePrField', async () => {
-  const result = await client.updateIssuePrField('PROJECT-1', 1, 'https://github.com/xxx/pull/1')
-  expect(result).toBe(false)
+describe('updateIssuePrField', () => {
+  it('failed to update', async () => {
+    const result = await client.updateIssuePrField('PROJECT-1', 1, 'https://github.com/xxx/pull/1')
+    expect(result).toBe(false)
+  })
 })
