1d1996fab850dcb46c814286358478189478dfa7
toshimaru <me@toshimaru.net>
2022-03-26 12:04:29 +0900

    Update jest.config.js
    ref. https://github.com/actions/typescript-action/blob/88a7452ac5a8c942a9b7c65df36070c952c4adc0/jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
   },
+  verbose: true
 }
