c0307dad74e4c488b57b602989641ecde8d1d7a6
toshimaru <me@toshimaru.net>
2022-04-29 19:51:28 +0900

    deno fmt src

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,9 @@
-import * as core from '@actions/core'
-import 'isomorphic-fetch'
-import 'isomorphic-form-data'
-import { Backlog } from 'backlog-js'
+import * as core from '@actions/core';
+import 'isomorphic-fetch';
+import 'isomorphic-form-data';
+import { Backlog } from 'backlog-js';
 
-const PR_FIELD_NAME = 'Pull Request'
+const PR_FIELD_NAME = 'Pull Request';
 
 interface CustomField {
   id: number;
@@ -15,114 +15,114 @@ export class Client {
   private host: string;
   private backlog: Backlog;
 
-  constructor (host: string, apiKey: string) {
-    this.host = host
-    this.backlog = new Backlog({ host, apiKey })
+  constructor(host: string, apiKey: string) {
+    this.host = host;
+    this.backlog = new Backlog({ host, apiKey });
   }
 
-  containsBacklogUrl (body: string): boolean {
-    return this.urlRegex.test(body)
+  containsBacklogUrl(body: string): boolean {
+    return this.urlRegex.test(body);
   }
 
-  parseBacklogUrl (body: string): Array<Array<string>> {
-    const urls: Array<Array<string>> = []
-    const urlRegex: RegExp = this.urlRegex
-    let matchData: Array<string> | null
+  parseBacklogUrl(body: string): Array<Array<string>> {
+    const urls: Array<Array<string>> = [];
+    const urlRegex: RegExp = this.urlRegex;
+    let matchData: Array<string> | null;
     while ((matchData = urlRegex.exec(body)) !== null) {
-      const [url, projectId, issueNo] = matchData
-      urls.push([url, projectId, `${projectId}-${issueNo}`])
+      const [url, projectId, issueNo] = matchData;
+      urls.push([url, projectId, `${projectId}-${issueNo}`]);
     }
-    return urls
+    return urls;
   }
 
-  async updateIssuePrField (
+  async updateIssuePrField(
     projectId: string,
     issueId: string,
-    prUrl: string
+    prUrl: string,
   ): Promise<boolean> {
     if (!await this.validateProject(projectId)) {
-      core.warning(`Invalid ProjectID: ${projectId}`)
-      return false
+      core.warning(`Invalid ProjectID: ${projectId}`);
+      return false;
     }
 
-    let prCustomField: CustomField | undefined
+    let prCustomField: CustomField | undefined;
     try {
-      prCustomField = await this.getPrCustomField(projectId)
+      prCustomField = await this.getPrCustomField(projectId);
     } catch (error) {
       if (error instanceof Error) {
-        core.error(error.message)
+        core.error(error.message);
       }
-      core.error('Failed to get custom field')
-      return false
+      core.error('Failed to get custom field');
+      return false;
     }
     if (prCustomField === undefined) {
-      core.warning('Skip process since "Pull Request" custom field not found')
-      return false
+      core.warning('Skip process since "Pull Request" custom field not found');
+      return false;
     }
 
-    let currentPrField: CustomField
+    let currentPrField: CustomField;
     try {
-      currentPrField = await this.getCurrentPrField(issueId, prCustomField.id)
+      currentPrField = await this.getCurrentPrField(issueId, prCustomField.id);
     } catch (error) {
       if (error instanceof Error) {
-        core.error(error.message)
+        core.error(error.message);
       }
-      core.warning(`Invalid IssueID: ${issueId}`)
-      return false
+      core.warning(`Invalid IssueID: ${issueId}`);
+      return false;
     }
     if ((currentPrField.value || '').includes(prUrl)) {
-      core.info(`Pull Request (${prUrl}) has already been linked`)
-      return false
+      core.info(`Pull Request (${prUrl}) has already been linked`);
+      return false;
     }
 
     try {
       const updateValue: string = currentPrField.value
         ? `${currentPrField.value}\n${prUrl}`
-        : prUrl
+        : prUrl;
       await this.backlog.patchIssue(issueId, {
-        [`customField_${currentPrField.id}`]: updateValue
-      })
-      return true
+        [`customField_${currentPrField.id}`]: updateValue,
+      });
+      return true;
     } catch (error) {
       if (error instanceof Error) {
-        core.error(error.message)
+        core.error(error.message);
       }
-      core.error('Failed to update')
-      return false
+      core.error('Failed to update');
+      return false;
     }
   }
 
-  async validateProject (projectId: string): Promise<boolean> {
+  async validateProject(projectId: string): Promise<boolean> {
     try {
-      await this.backlog.getProject(projectId)
-      return true
+      await this.backlog.getProject(projectId);
+      return true;
     } catch {
-      return false
+      return false;
     }
   }
 
-  async getPrCustomField (projectId: string): Promise<CustomField | undefined> {
+  async getPrCustomField(projectId: string): Promise<CustomField | undefined> {
     const fields: Array<CustomField> = await this.backlog.getCustomFields(
-      projectId
-    )
+      projectId,
+    );
     const prField: CustomField | undefined = fields.find(
-      (field: CustomField) => field.name === PR_FIELD_NAME
-    )
-    return prField
+      (field: CustomField) => field.name === PR_FIELD_NAME,
+    );
+    return prField;
   }
 
-  async getCurrentPrField (
+  async getCurrentPrField(
     issueId: string,
-    prFieldId: number
+    prFieldId: number,
   ): Promise<CustomField> {
-    const issue = await this.backlog.getIssue(issueId)
+    const issue = await this.backlog.getIssue(issueId);
     const prField: CustomField = issue.customFields.find(
-      (field: CustomField) => field.id === prFieldId
-    )
-    return prField
+      (field: CustomField) => field.id === prFieldId,
+    );
+    return prField;
   }
 
-  private get urlRegex (): RegExp {
-    return new RegExp(`https://${this.host}/view/(\\w+)-(\\d+)`, 'g')
+  private get urlRegex(): RegExp {
+    return new RegExp(`https://${this.host}/view/(\\w+)-(\\d+)`, 'g');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,35 @@
-import * as core from '@actions/core'
-import { context } from '@actions/github'
-import { Client } from './client'
+import * as core from '@actions/core';
+import { context } from '@actions/github';
+import { Client } from './client';
 
-async function main () {
+async function main() {
   try {
-    const host = core.getInput('backlog-host', { required: true })
-    const apiKey = core.getInput('backlog-api-key', { required: true })
+    const host = core.getInput('backlog-host', { required: true });
+    const apiKey = core.getInput('backlog-api-key', { required: true });
 
     if (context.payload.pull_request === undefined) {
-      throw new Error("Can't get pull_request payload. Check your trigger is pull_request event")
+      throw new Error('Can\'t get pull_request payload. Check your trigger is pull_request event');
     }
 
-    const client = new Client(host, apiKey)
-    const { html_url: prUrl = '', body = '' } = context.payload.pull_request
+    const client = new Client(host, apiKey);
+    const { html_url: prUrl = '', body = '' } = context.payload.pull_request;
     if (!client.containsBacklogUrl(body)) {
-      core.info("Skip process since the body doesn't contain backlog URL")
-      return
+      core.info('Skip process since the body doesn\'t contain backlog URL');
+      return;
     }
 
     for (const [backlogUrl, projectId, issueId] of client.parseBacklogUrl(body)) {
-      core.info(`Trying to link the Pull Request to ${backlogUrl}`)
+      core.info(`Trying to link the Pull Request to ${backlogUrl}`);
 
       if (await client.updateIssuePrField(projectId, issueId, prUrl)) {
-        core.info(`Pull Request (${prUrl}) has been successfully linked`)
+        core.info(`Pull Request (${prUrl}) has been successfully linked`);
       }
     }
   } catch (error) {
     if (error instanceof Error) {
-      core.setFailed(error.message)
+      core.setFailed(error.message);
     }
   }
 }
 
-main()
+main();
